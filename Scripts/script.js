3fa9aa12918f52befed1982c5890b835eab048c8
IGauto04 <63121360+zain-ali-12@users.noreply.github.com>
2022-03-18 18:59:10 +0400

    Ver 1

---
+++ Scripts/script.js
@@ -0,0 +1,17 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("hours").innerHTML = h;
+    document.getElementById("minutes").innerHTML = m;
+    document.getElementById("seconds").innerHTML = s;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
